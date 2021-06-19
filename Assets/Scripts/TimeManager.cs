99f60dba7911604da149bc7c82cff2ea77b27ec2
DevGa.me <devm19102003@gmail.com>
2021-06-19 17:30:19 +0530

    ADD : Timer System

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System;
+
+public enum TimerState { ON, OFF };
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager instance;
+
+    [SerializeField] float maxTime;
+    [SerializeField] float timeLeft;
+
+    [SerializeField] TimerState timerState = TimerState.OFF;
+
+    public void Start()
+    {
+        #region Maintain a single instance
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); }
+        #endregion
+
+        //TODO Do some sort of counting
+        timeLeft = maxTime;
+        timerState = TimerState.ON;
+    }
+
+    private void Update()
+    {
+        if (timerState == TimerState.ON)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+    }
+
+    public string GetTimeLeft()
+    {
+        string t;
+
+        t = Convert.ToString(Math.Round((double)timeLeft, 2));
+
+        return t;
+    }
+}
