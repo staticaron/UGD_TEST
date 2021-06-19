37b6a61e5b38b4d0ab0703959444e1a37a09ffed
DevGa.me <devm19102003@gmail.com>
2021-06-19 16:28:43 +0530

    ADD : Fruit Collection and doot linking

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -2,13 +2,33 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
+    private SpriteRenderer ren;
+
+    [SerializeField] ParticleSystem collectedParticle;
+    [SerializeField] bool isCollected;
+
+    private void Start()
+    {
+        ren = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            gameObject.SetActive(false);
-            //Spawn the Collected Effect
-            //Activate the door
+            if (isCollected == false)
+            {
+                //Disable the collectible
+                ren.enabled = false;
+
+                //Play Collected Animation
+                collectedParticle.Play();
+
+                //Open the door
+                Door.instance.OpenDoor();
+
+                isCollected = true;
+            }
         }
     }
 }
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,6 +4,11 @@ public class Door : MonoBehaviour
 {
     public static Door instance;
 
+    private const string openDoorAnimationName = "OpenDoor";
+    private Animator animator;
+
+    [SerializeField] bool isOpen = false;
+
     private void Start()
     {
         #region Maintain a single instance
@@ -11,6 +16,27 @@ public class Door : MonoBehaviour
         else { Destroy(gameObject); }
         #endregion
 
-        Debug.Log($"{instance.name}");
+        //References
+        animator = GetComponent<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+        animator.Play(openDoorAnimationName);
+        isOpen = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Check for the open door
+            if (isOpen)
+            {
+                //Check for time
+
+                Debug.Log("Next Level");
+            }
+        }
     }
 }
